ac35bd7fa4ccce426e564a67c2b56f02ca7f6021
pearceyj <86382179+pearceyj@users.noreply.github.com>
2022-07-11 18:38:15 -0700

    Separate into pages
    -site is separated into pages
    -each has a .js file for communication with user selections
    -I have no 404 page yet
    -no information page yet
    -Note: server only hosted locally at the moment

--- public/index.js
+++ public/index.js
@@ -1,11 +1,13 @@
-var locationButton = document.getElementById('locations-button')
-var settingsButton = document.querySelectorAll('settings-button')
+var locationsButton = document.getElementById('nav-locations-button')
+var settingsButton = document.getElementById('nav-settings-button')
 
 
 locationsButton.addEventListener('click', function(){
     window.location.href='/locations'
+    console.log("location button clicked")
 })
 
 settingsButton.addEventListener('click', function(){
     window.location.href='/settings'
+    console.log("settings button clicked")
 })
--- public/settings.js
+++ public/settings.js
@@ -1,5 +1,5 @@
 var backButton = document.getElementById('back-button')
 
 backButton.addEventListener('click', function(){
-    window.location.href='/index'
+    window.history.back()
 })
